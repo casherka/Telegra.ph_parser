b805207ed350e921c45dcef43413919ae852f4ee
casherka <76988015+casherka@users.noreply.github.com>
2024-10-22 20:24:57 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,137 +5,8 @@ import os
 import datetime
 from proxy_settings import proxy
 from proxy_settings import use_proxy
+from styles import style
 
-style = '''<style>
-    div header h1 {
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-weight: 700;
-        font-style: normal;
-        font-size: 32px;
-        margin: 21px 21px 12px;
-        line-height: 34px;
-        color: rgba(0, 0, 0, .8);
-    }
-
-    @media (max-width: 1365px) {
-    div header h1 {
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-weight: 700;
-        font-style: normal;
-        font-size: 24px;
-        margin: 21px 21px 12px;
-        line-height: 34px;
-        color: rgba(0, 0, 0, .8);
-    }}
-
-    div header address {
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-size: 15px;
-        line-height: 18px;
-        color: #79828B;
-        margin: 12px 21px;
-        font-style: normal;
-    }
-
-    @media (max-width: 1365px) {
-    div header address {
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-size: 12px;
-        line-height: 18px;
-        color: #79828B;
-        margin: 12px 21px;
-        font-style: normal;
-    }}
-
-    div p {
-        margin: 0 21px 12px;
-        word-wrap: break-word;
-        font-family: CustomSerif, Georgia, Cambria, 'Times New Roman', serif;
-        font-weight: 400;
-        font-style: normal;
-        font-size: 18px;
-        line-height: 1.58;
-        padding: 0;
-        color: rgba(0, 0, 0, .8);
-    }
-    
-    div {
-        max-width: 732px;
-        margin: 0 auto;
-        position: relative;
-        padding: 21px 0;
-        width: 100%;
-        flex-grow: 1;
-    }
-
-    @media (max-width: 1365px) {
-    div p {
-        margin: 0 21px 12px;
-        word-wrap: break-word;
-        font-family: CustomSerif, Georgia, Cambria, 'Times New Roman', serif;
-        font-weight: 400;
-        font-style: normal;
-        font-size: 18px;
-        line-height: 1.58;
-        padding: 0;
-        color: rgba(0, 0, 0, .8);
-        width:77%;
-    }}
-
-    @media (max-width: 1365px) {
-    div {
-        max-width: 732px;
-        margin: 0 20px;
-        position: relative;
-        padding: 21px 0;
-        width: 100%;
-        flex-grow: 1;
-    }}
-
-    div aside a {
-        position: fixed;
-        left: 47.2%;
-        top: 75px;
-        margin: 46px 0 0 376px;
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-weight: 600;
-        font-style: normal;
-        font-size: 17px;
-        color: #000;
-        text-align: center;
-        text-transform: uppercase;
-        padding: 0 0;
-        cursor: pointer;
-        width: 350px;
-        text-decoration: unset;
-        background-color: #ffffff00;
-    }
-
-    @media (max-width: 1365px){
-    div aside a {
-        position: fixed;
-        left: 23%;
-        top: 75px;
-        right: 0;
-        margin: 46px 0 0 376px;
-        font-family: CustomSansSerif, 'Lucida Grande', Arial, sans-serif;
-        font-weight: 600;
-        font-style: normal;
-        font-size: 17px;
-        color: #000;
-        text-align: center;
-        text-transform: uppercase;
-        padding: 0 0;
-        cursor: pointer;
-        width: 350px;
-        text-decoration: unset;
-        background-color: #ffffff00;
-    }}
-    
-    div header address a {
-        margin-right: 12px;
-    }
-</style>'''
 
 proxies_set = {
   "http": proxy,
@@ -183,8 +54,7 @@ def send_request(title, month, day,  folder_path, using_proxy: bool = False):
 
     if request.status_code == 200:
         with open(f"{folder_path}/{title}-{month}-{day}.html", "w", encoding='utf-8') as file:
-            file.write('<div>' + str(soup.header) +
-                       F"{''.join(map(str, soup.article.find_all('p')))}"
+            file.write('<div>' + str(soup) +
                        F"<aside><a href='https://telegra.ph/{title}-{month}-{day}'>"
                        F"|Online| https://telegra.ph/{title}-{month}-{day}</a></aside>"
                        F"</div>\n" + style)
